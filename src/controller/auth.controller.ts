ff0a86c4753f79b66a6034450312045b2cee1952
HemanthSai k <khemanthsai01@gmail.com>
2023-08-17 17:26:24 +0530

    change in  auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -6,10 +6,5 @@ export const signUp = (req: Request, res: Response) => {
 };
 
 export const signIn = (req: Request, res: Response) => {
-  const result = validationResult(req);
-  if (result.isEmpty()) {
-    res.send("user is singing in ");
-  } else {
-    res.json({ errors: result.array() });
-  }
+  res.send("user is singing in ");
 };
